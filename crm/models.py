7f511c2c2490167306e82c6a31c754ca8ac888d0
kristof <sichkardenis5@gmail.com>
2022-12-02 11:40:15 +0200

    :link: Connect Orders model to admin panel

--- crm/models.py
+++ crm/models.py
@@ -2,6 +2,13 @@ from django.db import models
 
 
 class Order(models.Model):
-    order_dt = models.DateTimeField(auto_now=True)
-    order_name = models.CharField(max_length=200)
-    order_phone = models.CharField(max_length=200)
+    order_dt = models.DateTimeField(auto_now=True, verbose_name='date')
+    order_name = models.CharField(max_length=200, verbose_name='name')
+    order_phone = models.CharField(max_length=200, verbose_name='phone')
+
+    def __str__(self):
+        return self.order_name
+
+    class Meta:
+        verbose_name = "Order"
+        verbose_name_plural = "Orders"
