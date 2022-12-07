1ceef87563e67005544a6717d39c8681d5b58d20
kristof <sichkardenis5@gmail.com>
2022-12-07 14:28:37 +0200

    :sparkles: Add Statuses and Comments to CRM

--- crm/models.py
+++ crm/models.py
@@ -1,10 +1,26 @@
 from django.db import models
 
 
+class StatusCRM(models.Model):
+    status_name = models.CharField(max_length=200, verbose_name='Status Name')
+
+    def __str__(self):
+        return self.status_name
+
+    class Meta:
+        verbose_name = "Status"
+        verbose_name_plural = "Statuses"
+
+
 class Order(models.Model):
-    order_dt = models.DateTimeField(auto_now=True, verbose_name='date')
-    order_name = models.CharField(max_length=200, verbose_name='name')
-    order_phone = models.CharField(max_length=200, verbose_name='phone')
+    order_dt = models.DateTimeField(auto_now=True, verbose_name='Date')
+    order_name = models.CharField(max_length=200, verbose_name='Name')
+    order_phone = models.CharField(max_length=200, verbose_name='Phone')
+    order_status = models.ForeignKey(
+        to=StatusCRM, on_delete=models.PROTECT,
+        null=True, blank=True,
+        verbose_name='Status'
+    )
 
     def __str__(self):
         return self.order_name
@@ -12,3 +28,19 @@ class Order(models.Model):
     class Meta:
         verbose_name = "Order"
         verbose_name_plural = "Orders"
+
+
+class CommentCRM(models.Model):
+    comment_binding = models.ForeignKey(
+        to=Order, on_delete=models.CASCADE,
+        verbose_name='Order'
+    )
+    comment_text = models.TextField(verbose_name='Comment Text', max_length=300)
+    comment_date = models.DateTimeField(auto_now=True, verbose_name='Creation Date')
+
+    def __str__(self):
+        return self.comment_text
+
+    class Meta:
+        verbose_name = "Comment"
+        verbose_name_plural = "Comments"
